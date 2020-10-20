223c033e631d658e2f565a751b09a0d9397d3cbe
Jamie Evans <jamieevans96@googlemail.com>
2020-10-20 10:26:14 +0100

    Updates

---
+++ precache-manifest.71628c28a31fed4ad91c4884c540e448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c275edb552a6d349a3b038e68389f4d8",
+    "url": "/CV-Builder/index.html"
+  },
+  {
+    "revision": "bf6e02193958038de65e",
+    "url": "/CV-Builder/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "bf6e02193958038de65e",
+    "url": "/CV-Builder/static/js/2.e13f1df8.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/CV-Builder/static/js/2.e13f1df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f07d1b52d306436e5a",
+    "url": "/CV-Builder/static/js/main.9240b30a.chunk.js"
+  },
+  {
+    "revision": "e641445fea2abcbe05d7",
+    "url": "/CV-Builder/static/js/runtime-main.06ada664.js"
+  }
+]);
